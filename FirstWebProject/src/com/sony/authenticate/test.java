6ec325e2dacaa4b20bfc16e25f5c8451693ce836
7000017531 <7000017531@INSISCILT-3216.ap.sony.com>
2018-08-31 09:50:29 +0530

    initial commit
    HTML
    CSS

---
+++ FirstWebProject/src/com/sony/authenticate/test.java
@@ -0,0 +1,52 @@
+package com.sony.authenticate;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class test
+ */
+@WebServlet("/test")
+public class test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String ud = request.getParameter("userid");
+		String pss = request.getParameter("pss");
+		
+		if(ud.equalsIgnoreCase("sheen") && pss.equals("1234"))
+		{
+			response.getWriter().println("hello sheen");
+		}else{
+			response.getWriter().println("hello bendekai");
+		}
+		
+		
+	}
+
+}
